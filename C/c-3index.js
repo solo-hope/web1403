cf18f0714b84bce00c50724db424a8ae124c12a3
solo-hope <omidtoosi.com@gmail.com>
2024-10-19 22:33:23 +0330

    Create c-3index.js

---
+++ C/c-3index.js
@@ -0,0 +1,56 @@
+let cmd = require('./B-4-cmd.js');
+let fs = require('fs');
+
+cmd.use("minus", function(contInputs){
+    console.log(cmd.parseInput(contInputs[1]) - cmd.parseInput(contInputs[2]));
+});
+cmd.use("sum", function(contInputs){
+    console.log(cmd.parseInput(contInputs[1]) + cmd.parseInput(contInputs[2]));
+});
+cmd.use("multiply", function(contInputs){
+    console.log(cmd.parseInput(contInputs[1]) * cmd.parseInput(contInputs[2]));
+});
+cmd.use("div", function(contInputs){
+    console.log(cmd.parseInput(contInputs[1]) / cmd.parseInput(contInputs[2]));
+});
+cmd.use("printRecord", function(contInputs){
+    console.log({
+        name: contInputs[1],
+        family: contInputs[2],
+        age: contInputs[3],
+        email: contInputs[4]
+    });
+});
+
+cmd.use("saveRecord", function(contInputs){
+    let dbWrite = {
+        name : contInputs[1],
+        family : contInputs[2],
+        age : contInputs[3],
+        email : contInputs[4]
+    }
+
+    let formattedData = JSON.stringify(dbWrite)
+
+    fs.writeFile('myDatabase.txt', formattedData, {encoding:'utf8'}, function(error){
+        if(error){
+            console.log('ERROR:', error);
+        }
+        else{
+            console.log('File Saved.');
+        }
+    })
+});
+
+cmd.use("openFile", function(contInputs){
+    fs.readFile(contInputs[1], function(error, data){
+        if (error){
+            console.log("ERROR : ", error);
+        }
+        else {
+            console.log("Success : ", data.toString());
+        }
+    })
+})
+
+cmd.start();
